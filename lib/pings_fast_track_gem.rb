61ff2b74248f81a2647c01d71f34392cb8686e4b
PING NGE <ping.nge@hotmail.com>
2020-03-10 14:36:19 +1100

    initial version

---
+++ lib/pings_fast_track_gem.rb
@@ -0,0 +1,6 @@
+require "pings_fast_track_gem/version"
+
+module PingsFastTrackGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pings_fast_track_gem/version.rb
@@ -0,0 +1,3 @@
+module PingsFastTrackGem
+  VERSION = "0.1.0"
+end
