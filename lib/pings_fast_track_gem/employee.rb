1f96fe050e4b041db2efb3a9854d3197f49ce6e4
PING NGE <ping.nge@hotmail.com>
2020-03-10 16:47:01 +1100

    added employee class, added menu

---
+++ lib/pings_fast_track_gem/employee.rb
@@ -0,0 +1,12 @@
+
+
+class Employee
+  attr_accessor
+
+  def initialize(name, age, gender)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+
+end
---
+++ lib/pings_fast_track_gem/menu.rb
@@ -0,0 +1,25 @@
+
+def menu
+  loop do
+  puts "Please choose from the following options"
+  puts "1: Creating a new profile"
+  puts "2: Update existing profile"
+  puts "3: Assign a roster to an employee"
+  puts "4: Exit the app"
+  print "--" * 27
+  print ""
+  print ">>"
+  selection = gets.chomp.to_i
+
+  case selection
+    when 1
+    when 2
+    when 3
+    else 4
+      puts "Thank you for using our service"
+      exit
+    end
+  end
+end
+ 
+def 
