3fe9f0fb2f8522a7742937bf3d6341e7d9a876da
PING NGE <ping.nge@hotmail.com>
2020-03-12 11:13:51 +1100

    fully implemented tty-prompt, formated code

--- lib/pings_fast_track_gem/menu.rb
+++ lib/pings_fast_track_gem/menu.rb
@@ -1,31 +1,6 @@
 require_relative "version"
 employees = []
 
-# def menu(employees)
-#   loop do
-#   puts "Please choose from the following options"
-#   puts "1: Creating a new profile"
-#   puts "2: Update or Remove existing profile"
-#   puts "3: Assign a roster to an employee"
-#   puts "4: Exit the app"
-#   print "--" * 27
-#   print ""
-#   print ">>"
-#   selection = gets.chomp.to_i
-#   case selection
-#     when 1 
-#       create_profile(employees)
-#     when 2
-#       update_profile(employees)
-#     when 3
-#       add_shift()
-#     else 4
-#       puts "Thank you for using our service"
-#       exit
-#     end
-#   end
-# end
-
 def read_employee_csv
   csv_text = File.read('database.csv')
   csv = CSV.parse(csv_text, headers: true)
@@ -35,24 +10,27 @@ def read_employee_csv
   end 
 end
 
-# menu(read_employee_csv)
-
-def test(data)
+def menu(data)
   prompt = TTY::Prompt.new
   system("clear")
-  choices = ['Create New Employee', 'Update Existing Profile', 'Assign Shifts', 'Exit Application']
-  user_selection = prompt.select('Please select from the following options', choices)
+  user_selection = prompt.select('Please select from the following options') do |menu|
+    menu.choice 'Create New Employee', 1
+    menu.choice 'Update Existing Profile', 2
+    menu.choice 'Assign Shifts', 3
+    menu.choice 'Exit Application', 4
+  end
 
-  if user_selection == 'Create New Employee'
+  case user_selection
+  when 1
     create_profile
-  elsif user_selection == 'Update Existing Profile'
+  when 2
     update_profile(data)
-  elsif user_selection == 'Assign Shifts'
+  when 3
     add_shift
-  else user_selection == 'Exit Application'
+  when 4
     puts "Thank you for using our service"
     exit
   end
 end
 
-test(read_employee_csv)
+menu(read_employee_csv)
--- lib/pings_fast_track_gem/methods.rb
+++ lib/pings_fast_track_gem/methods.rb
@@ -1,24 +1,25 @@
 require_relative "version"
 
 employee_profile = []
+$prompt = TTY::Prompt.new
 
 def create_profile
   system "clear"
-  f_name = prompt.ask("Enter first name:") do |q|
+  f_name = $prompt.ask("Enter first name:") do |q|
     q.required(true)
     q.validate /\A\w+\Z/
     q.modify   :capitalize
   end
 
-  l_name = prompt.ask("Enter last name:") do |q|
+  l_name = $prompt.ask("Enter last name:") do |q|
     q.required(true)
     q.validate /\A\w+\Z/
     q.modify   :capitalize
   end
 
-  Age = prompt.ask("Enter age:")
+  age = $prompt.ask("Enter age:")
 
-  gender = prompt.ask("Enter gender") do |q|
+  gender = $prompt.ask("Enter gender") do |q|
     q.required(true)
     q.validate /\A\w+\Z/
     q.modify   :capitalize
@@ -31,8 +32,7 @@ end
 
 def update_profile(employees)
   system "clear"
-  prompt = TTY::Prompt.new
-  name_input = prompt.ask("Which employee profile would you like to update?") do |q|
+  name_input = $prompt.ask("Which employee profile would you like to update?") do |q|
     q.required(true)
     q.validate /\A\w+\Z/
     q.modify   :capitalize
@@ -40,8 +40,9 @@ def update_profile(employees)
   found = employees.detect do |employee|
   employee.first_name == name_input
   end
+
   if found
-  selection = prompt.select('Please select from the following options') do |menu|
+  selection = $prompt.select('Please select from the following options') do |menu|
     menu.enum "."
     menu.choice 'Update First Name', 1
     menu.choice 'Update Last Name', 2
@@ -50,26 +51,26 @@ def update_profile(employees)
   end
     case selection
       when 1
-      user_input = prompt.ask("Enter new first name: ") do |q|
+      user_input = $prompt.ask("Enter new first name:") do |q|
         q.required(true)
         q.validate /\A\w+\Z/
         q.modify   :capitalize
       end
       found.first_name = user_input
       when 2
-      user_input = prompt.ask("Enter new last name: ") do |q|
+      user_input = $prompt.ask("Enter new last name:") do |q|
         q.required(true)
         q.validate /\A\w+\Z/
         q.modify   :capitalize
       end
-      found.first_name = user_input
+      found.last_name = user_input
       when 3
-      user_input = prompt.ask("Enter new age: ") do |q|
+      user_input = $prompt.ask("Enter new age:") do |q|
         q.required(true)
       end
       found.age = user_input
       when 4
-      user_input = prompt.ask("Enter new gender: ") do |q|
+      user_input = $prompt.ask("Enter new gender:") do |q|
         q.required(true)
         q.validate /\A\w+\Z/
         q.modify   :capitalize
@@ -93,11 +94,10 @@ end
 
 def add_shift
   system "clear"
-  prompt = TTY::Prompt.new
   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-  day = prompt.select("Please select a day: ", days, filter: true)
+  day = $prompt.select("Please select a day: ", days, filter: true)
 
-  name = prompt.ask('Please input the name of the employee: ') do |q|
+  name = $prompt.ask('Please input the name of the employee: ') do |q|
     q.required(true)
     q.validate /\A\w+\Z/
     q.modify   :capitalize
@@ -106,9 +106,9 @@ def add_shift
   times = [
     '12:00am', '1:00am', '2:00am', '3:00am', '4:00am', '5:00am', '6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:30am', '1:30am', '2:30am', '3:30am', '4:30am', '5:30am', '6:30am', '7:30am', '8:30am', '9:30am', '10:30am', '11:30am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm', '10:00pm', '11:00pm', '12:30pm', '1:30pm', '2:30pm', '3:30pm', '4:30pm', '5:30pm', '6:30pm', '7:30pm', '8:30pm', '9:30pm', '10:03pm', '11:30pm'
   ]
-  s_time = prompt.select('Please select start time: ', times, filter: true)
+  s_time = $prompt.select('Please select start time: ', times, filter: true)
 
-  e_time = prompt.select('Please select start time: ', times, filter: true)
+  e_time = $prompt.select('Please select start time: ', times, filter: true)
 
   CSV.open("shift.csv", "ab") do |csv|
     csv << [day, name, s_time, e_time]
